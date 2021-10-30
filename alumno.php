ce8da300b776a971457baa0b98b03c1914cef3e2
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-30 13:49:54 -0500

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -27,17 +27,7 @@ include("conexion.php");
   padding: 8px;
 }
 
-#customers tr:nth-child(even){background-color: #f2f2f2;}
 
-#customers tr:hover {background-color: #ddd;}
-
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #04AA6D;
-  color: white;
-}
 </style>
     </head>
     <body>
