8dc326544afd7a2011cab4e6444a1d6833f6a493
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-30 13:19:39 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,9 +2,10 @@
 function conectar(){
     $host="ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $user="nxigc6de0c0eeqdh";
-    $pass="	lm3rubq7gzasfqk3";
+    $pass="lm3rubq7gzasfqk3";
 
     $bd="ihfuw8gt8ymcenf5";
+   
 
     $con=mysqli_connect($host,$user,$pass);
 
