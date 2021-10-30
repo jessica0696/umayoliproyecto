9e8c36c8a4a1a2b723a2a628823988fa7c18e949
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-30 12:38:31 -0500

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -13,6 +13,7 @@ include("conexion.php");
         <title> PAGINA ALUMNO</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     </head>
