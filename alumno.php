597c5736e88a45c58f1dd41203248bca5458981f
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-30 12:29:31 -0500

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -1,5 +1,11 @@
 <?php
+include("conexion.php");
+    $con=conectar();
 
+    $sql="SELECT *  FROM alumno";
+    $query=mysqli_query($con,$sql);
+
+    $row=mysqli_fetch_array($query);
 ?>
 <!DOCTYPE html>
 <html lang="en">
