e66f0131fc95188988a6a2a8c96832cf22b2bf04
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-30 12:35:26 -0500

    css delete

--- alumno.php
+++ alumno.php
@@ -13,7 +13,6 @@ include("conexion.php");
         <title> PAGINA ALUMNO</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     </head>
