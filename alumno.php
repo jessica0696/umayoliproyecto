375f3ce8f6c9bbb9c98f09a6e64c659c370de9b0
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-30 13:47:10 -0500

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -15,18 +15,41 @@ include("conexion.php");
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <style>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
     </head>
     <body>
            
         <h1>Usuarios: </h1>
                         <div class="col-md-8">
-                            <table class="table" >
-                                <thead class="thead-dark" >
+                            <table class="table" id="customers">
+                                <thead style >
                                     <tr>
-                                        <th scope="col">Codigo</th>
-                                        <th scope="col">Nombre</th>
-                                        <th scope="col">Apellido</th>
+                                        <th>Codigo</th>
+                                        <th>Nombre</th>
+                                        <th>Apellido</th>
                                         <th></th>
                                         <th></th>
                                     </tr>
