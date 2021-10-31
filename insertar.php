c3d1982d28272776fb10bbb89f87fee425838780
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-31 10:55:53 -0500

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -3,11 +3,12 @@ include("conexion.php");
 $con=conectar();
 
 $cod_estudiante=$_POST['cod_estudiante'];
+$dni=$_POST['dni'];
 $nombres=$_POST['nombres'];
 $apellidos=$_POST['apellidos'];
 
 
-$sql="INSERT INTO alumno VALUES('$cod_estudiante','$nombres','$apellidos')";
+$sql="INSERT INTO alumno VALUES('$cod_estudiante','$dni','$nombres','$apellidos')";
 $query= mysqli_query($con,$sql);
 
 if($query){
