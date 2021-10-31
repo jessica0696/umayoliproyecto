0fd8d811024da64a52307384532d6b0a18052e99
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-10-31 10:58:05 -0500

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -10,7 +10,7 @@ include("conexion.php");
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title> PAGINA ALUMNO</title>
+        <title> Usuarios</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
