dabc3b346a0c73c7fd82f6000dab1c908c2991a7
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-11-12 10:29:49 -0600

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -304,23 +304,14 @@ p {
   
                         
                         
-                        <footer class="site-footer">
+ <footer class="site-footer">
       <div class="container">
       <div class="container">
         <div class="row">
           <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">Scanfcode</a>.
+            <p class="copyright-text">Conceptualización de servicios en la nube-
+Producto Integrador- desarrollado por Jessica Arambula
             </p>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
           </div>
         </div>
       </div>
