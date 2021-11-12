ddd0caf5addf77aec2c3390fec724ab33c56f425
jessica0696 <63938881+jessica0696@users.noreply.github.com>
2021-11-12 12:00:10 -0600

    Update alumno.php

--- alumno.php
+++ alumno.php
@@ -17,6 +17,14 @@ include("conexion.php");
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <style>
             @import url(https://fonts.googleapis.com/css?family=Lato:400,300,100,700,900);
+            div {
+  width: 100%;
+  height: 400px;
+  background-image: url('usuario.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  border: 1px solid red;
+}
 
 h1,
 p,
